2323017a7f00f808a648a876f08325bacbf0ca83
konstantin.kiess <konstantin.kiess@gmx.de>
2020-01-14 15:33:52 +0100

    added s3 encryption

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -6,6 +6,17 @@ resource "aws_s3_bucket" "env-state-bucket" {
     Name        = "${var.TLD}-state"
     Environment = "${var.TLD}-state"
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.kops_state.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
 
-
+resource "aws_kms_key" "kops_state" {
+  description             = "Kops State Encryption Key for ${var.TLD}"
+  deletion_window_in_days = 7
+}
